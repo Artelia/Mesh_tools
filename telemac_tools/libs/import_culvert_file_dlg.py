0a3eebf7bc12baf9880d914da0385eca46cda8d7
nicogodet <nicolas.godet@outlook.fr>
2021-11-23 15:04:21 +0100

    Use combobox to map parameters

--- telemac_tools/libs/import_culvert_file_dlg.py
+++ telemac_tools/libs/import_culvert_file_dlg.py
@@ -4,7 +4,7 @@ import os
 
 from qgis.PyQt import uic
 from qgis.PyQt.QtCore import QCoreApplication
-from qgis.PyQt.QtWidgets import QDialog, QTableWidgetItem
+from qgis.PyQt.QtWidgets import QComboBox, QDialog, QTableWidgetItem
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), "..", "ui", "import_culvert_file.ui"))
 
@@ -16,7 +16,6 @@ class dlg_import_culvert_file(QDialog, FORM_CLASS):
 
         self.items = {}
         for fld in culv_flds:
-            self.tableWidget.insertRow(self.tableWidget.rowCount())
             self.items[fld.lower()] = [fld, ""]
 
         self.updateTable()
@@ -27,22 +26,47 @@ class dlg_import_culvert_file(QDialog, FORM_CLASS):
         self.text_file.fileChanged.connect(self.parseTextFile)
         self.cb_tel_ver.currentIndexChanged.connect(self.parseTextFile)
 
+        self.buttonBox.accepted.connect(self.dialogAccepted)
+        self.buttonBox.rejected.connect(self.dialogRejected)
+
     def tr(self, message):
         return QCoreApplication.translate(self.__class__.__name__, message)
 
+    def cleanTable(self):
+        while self.tableWidget.rowCount() > 0:
+            self.tableWidget.cellWidget(0, 1).currentIndexChanged.disconnect()
+            self.tableWidget.removeRow(0)
+
     def updateTable(self):
-        self.tableWidget.clearContents()
-        for i, [param, txt_param] in enumerate(self.items.values()):
+        self.cleanTable()
+
+        txt_params = [""]
+        for x in self.items.values():
+            if x[1]:
+                txt_params.append(x[1])
+
+        for param, txt_param in self.items.values():
+            self.tableWidget.insertRow(self.tableWidget.rowCount())
             self.tableWidget.setItem(
-                i,
+                self.tableWidget.rowCount() - 1,
                 0,
                 QTableWidgetItem(param),
             )
-            self.tableWidget.setItem(
-                i,
+            cb = QComboBox()
+            cb.addItems(txt_params)
+            cb.setCurrentText(txt_param)
+            cb.currentIndexChanged.connect(self.updateDico)
+            self.tableWidget.setCellWidget(
+                self.tableWidget.rowCount() - 1,
                 1,
-                QTableWidgetItem(txt_param),
+                cb,
             )
+
+    def updateDico(self):
+        for row in range(self.tableWidget.rowCount()):
+            param = self.tableWidget.item(row, 0).text()
+            txt_param = self.tableWidget.cellWidget(row, 1).currentText()
+            self.items[param.lower()][1] = txt_param
 
     def parseTextFile(self):
         def get_values(string):
@@ -66,3 +90,12 @@ class dlg_import_culvert_file(QDialog, FORM_CLASS):
                 self.items[header.lower()][1] = header
 
         self.updateTable()
+
+    def dialogAccepted(self):
+        self.updateDico()
+        self.cleanTable()
+        self.accept()
+
+    def dialogRejected(self):
+        self.cleanTable()
+        self.reject()
